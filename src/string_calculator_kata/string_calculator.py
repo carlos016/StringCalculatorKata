a2b452f1bee3f97ac54fd2195641ada94bbda51a
Carlos García González <alu0100898026@ull.edu.es>
2025-10-29 23:53:29 +0000

    Modify StringCalculator.add to add more than 2 numbers

--- src/string_calculator_kata/string_calculator.py
+++ src/string_calculator_kata/string_calculator.py
@@ -2,21 +2,16 @@ class StringCalculator:
     def add(self, numbers: str = "") -> int:
         total = 0
         if numbers:
-            if numbers.count(",") >= 2:
-                raise ValueError(
-                    "Invalid input: More than two numbers provided."
-                    " Only up to two numbers are allowed."
-                )
-            for number in numbers.split(","):
-                try:
+            try:
+                for number in numbers.split(","):
                     total += int(number)
-                except ValueError:
-                    print(
-                        f"Invalid input encountered: {number},"
-                        f" only numbers are allowed."
-                    )
-                    raise ValueError(
-                        f"Invalid input: {number}, only numbers are allowed."
-                    )
+            except ValueError:
+                print(
+                    f"Invalid input encountered: {number},"
+                    f" only numbers are allowed."
+                )
+                raise ValueError(
+                    f"Invalid input: {number}," f" only numbers are allowed."
+                )
 
         return total
