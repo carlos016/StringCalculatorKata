66f292c3f91b07e84ede52f9532b4d23a229554b
Carlos García González <alu0100898026@ull.edu.es>
2025-10-29 11:08:37 +0000

    Add test for add empty string

--- tests/test_string_calculator_kata.py
+++ tests/test_string_calculator_kata.py
@@ -3,22 +3,18 @@ import pytest
 
 """Tests for `string_calculator_kata` package."""
 
-# from string_calculator_kata import string_calculator_kata
+from string_calculator_kata import StringCalculator
 
 
+# Before each test, we create a new instance of StringCalculator
 @pytest.fixture
-def response() -> None:
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get(
-    # 'https://github.com/audreyfeldroy/cookiecutter-pypackage'
-    # )
+def before_each() -> StringCalculator:
+    """Función que se ejecuta antes de cada prueba."""
+    # Configuración inicial
+    return StringCalculator()
 
 
-def test_content(response: None) -> None:
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+# Test adding an empty string
+def test_add_empty_string(before_each: StringCalculator) -> None:
+    calculator = before_each
+    assert calculator.add("") == 0
