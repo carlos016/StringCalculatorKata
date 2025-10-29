ea662356fbec10ca465ac271a0186f0595257548
Carlos García González <alu0100898026@ull.edu.es>
2025-10-29 12:07:34 +0000

    Modify add method to work with non empty string

--- src/string_calculator_kata/string_calculator.py
+++ src/string_calculator_kata/string_calculator.py
@@ -1,6 +1,6 @@
 class StringCalculator:
     def add(self, numbers: str = "") -> int:
         if numbers != "" and numbers != "0":
-            return -1
+            return sum(int(num) for num in numbers.split(","))
 
         return 0
