2f50e790d5a4995d939eee54ec6bfc7923029541
Carlos García González <alu0100898026@ull.edu.es>
2025-10-29 23:44:24 +0000

    Modify add method to handling errors

--- src/string_calculator_kata/string_calculator.py
+++ src/string_calculator_kata/string_calculator.py
@@ -3,6 +3,15 @@ class StringCalculator:
         total = 0
         if numbers:
             for number in numbers.split(","):
-                total += int(number)
+                try:
+                    total += int(number)
+                except ValueError:
+                    print(
+                        f"Invalid input encountered: {number},"
+                        f" only numbers are allowed."
+                    )
+                    raise ValueError(
+                        f"Invalid input: {number}, only numbers are allowed."
+                    )
 
         return total
